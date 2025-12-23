addcf0766e7c779da11c2df1616702c15d7d07f2
MarkBelgum <mtbelgum@gmail.com>
2025-12-23 15:49:06 -0600

    sdf

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, createContext, useContext } from 'react';
+import { useState, useEffect, useRef, createContext } from 'react';
 import { Menu, X, Heart, Target, Users, Book, ChevronRight, Sun, Moon } from 'lucide-react';
 import './App.css';
 
